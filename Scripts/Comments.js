1dbbd36c61c72a8b5e6fddf46b407871aeff6580
TerraArrs <166519103+TerraArrs@users.noreply.github.com>
2024-04-20 01:14:45 +0600

    Add files via upload

---
+++ Scripts/Comments.js
@@ -0,0 +1,15 @@
+function Kom(){
+    const New = document.createElement('div');
+    New.className = 'PB'
+    const New1 = document.createElement('h1');
+    const New2 = document.createElement('p');
+
+    const New1TXT = document.createTextNode('213');
+    New1.appendChild(New1TXT);
+    document.body.appendChild(New1);
+
+    const txt1 = document.getElementById('MT').value;
+    const txt2 = document.getElementById('MTT').value;
+    
+
+}
---
+++ Scripts/Index.js
@@ -0,0 +1,6 @@
+const url1 = "file:///C:/ospanel/domains/tts/WK.html"
+
+function Comp(){
+    
+    window.location.href = url1;
+}
